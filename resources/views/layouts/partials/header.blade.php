d092013df28b13b093cd69d303d4f742228ca41a
BosungKim2697 <qhtjdrla@gmail.com>
2018-11-27 13:58:20 +0900

    SW latex

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,51 @@
+<!--[if IE 9]>         <html class="ie9 no-focus" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-focus" lang="en"> <!--<![endif]-->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('app.name', '') }}</title>
+
+<!-- Styles -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+<!-- Scripts -->
+
+
+<title>Lab-Manager</title>
+
+<meta name="description" content="Lab-Manager">
+<meta name="author" content="Lab-Manager">
+<meta name="robots" content="noindex, nofollow">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Lab-Manager">
+<meta property="og:description" content="We bring an easy blockchain to your business">
+<meta property="og:url" content="http://www.primeblock.co/">
+<meta property="og:image" content="http://www.primeblock.co/static/assets/img/pb_logo_preview.png">
+<meta property="og:image:width" content="800">
+<meta property="og:image:height" content="400">
+<link rel="shortcut icon" href="../static/favicon_32.png">
+
+<!-- Stylesheets -->
+<!-- Web fonts -->
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700">
+
+<!-- Bootstrap and OneUI CSS framework -->
+<link rel="stylesheet" href="../static/assets/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="../static/assets/css/bootstrap.min.css">
+<link rel="stylesheet" id="css-main" href="../static/assets/css/oneui.css">
+<link rel="stylesheet" id="css-customize" href="../static/assets/css/style.css?ver=2018082112">
+
+<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,600,700%7COpen+Sans:300,400,400italic,600,700">
+
+<!-- Bootstrap and OneUI CSS framework -->
+<link rel="stylesheet" href="../static/assets/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="../static/assets/css/bootstrap.min.css">
+<link rel="stylesheet" id="css-main" href="../static/assets/css/oneui.css">
+<link rel="stylesheet" id="css-customize" href="../static/assets/css/style.css?ver=2018082112">
