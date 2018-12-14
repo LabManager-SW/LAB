a471cafac74c3c6f7ff6811e99a32b6566f2d31c
BosungKim2697 <qhtjdrla@gmail.com>
2018-12-14 14:47:07 +0900

    small errors fixed

--- resources/views/user/mypage/show.blade.php
+++ resources/views/user/mypage/show.blade.php
@@ -32,16 +32,9 @@
                 </div>
                 <div class="form-group">
                     <label for="tester_name">담당 연구원</label>
-                    <input type="text" id="tester_name" class="form-control" value="{{$tester->name}}" disabled>
+                    <input type="text" id="tester_name" class="form-control" value="{{$value->tester_name}}" disabled>
                 </div>
-                <div class="form-group">
-                    <label for="tester_email">담당 연구원 이메일</label>
-                    <input type="email" id="tester_email" class="form-control" value="{{$tester->email}}" disabled>
-                </div>
-                <div class="form-group">
-                    <label for="tester_phone">담당 연구원 연락처</label>
-                    <input type="email" id="tester_phone" class="form-control" value="{{$tester->phone}}" disabled>
-                </div>
+               
             </div>
             <!-- End 실험정보 -->
         </div>
